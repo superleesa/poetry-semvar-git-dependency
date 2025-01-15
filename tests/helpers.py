a6fe71021381a4ea8747f20954923d147e5ea6a8
Satoshi <6world4trigger@gmail.com>
2025-01-16 04:29:07 +0900

    add constants / helpers for plugin test

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,6 +1,15 @@
+from unittest.mock import Mock
+
 from pathlib import Path
+
 from dulwich.repo import Repo
 from dulwich.porcelain import tag_create, commit, add
+from poetry.poetry import Poetry
+from poetry.core.packages.project_package import ProjectPackage
+from poetry.core.packages.dependency_group import DependencyGroup
+from poetry.core.packages.dependency import Dependency
+
+from tests.mock_constants import MOCK_PROJECT_NAME, MOCK_PROJECT_VERSION
 
 
 def create_file_and_tag_commit(
@@ -27,3 +36,35 @@ def create_file_and_tag_commit(
         objectish=commit_id,
         message=f"Mock tag {tag_name}".encode(),
     )
+
+
+class FakePoetry(Poetry):
+    """
+    This assumes that we are not interested in configs / lockers
+    """
+    def __init__(self, file: Path, package: ProjectPackage) -> None:
+        mock_config = Mock()
+        mock_locker = Mock()
+        super().__init__(file, {}, package, mock_locker, mock_config)
+
+
+def create_project_package_from_raw_dep_groups(
+    raw_dep_groups: dict[str, list[Dependency]],
+) -> ProjectPackage:
+    dep_groups = create_dep_groups_from_raw_group_deps(raw_dep_groups)
+    project_package = ProjectPackage(MOCK_PROJECT_NAME, MOCK_PROJECT_VERSION)
+    for group_name, group in dep_groups.items():
+        project_package.add_dependency_group(group)
+    return project_package
+
+
+def create_dep_groups_from_raw_group_deps(
+    deps: dict[str, list[Dependency]],
+) -> dict[str, DependencyGroup]:
+    dep_groups = {}
+    for group_name, group_deps in deps.items():
+        group = DependencyGroup(name=group_name)
+        for dep in group_deps:
+            group.add_dependency(dep)
+        dep_groups[group_name] = group
+    return dep_groups
---
+++ tests/mock_constants.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+
+MOCK_PROJECT_NAME = "mock-project"
+MOCK_PROJECT_FILE_PATH = Path(MOCK_PROJECT_NAME)
+MOCK_PROJECT_VERSION = "1.0.0"
+MOCK_GIT_SOURCE_URL = "https://github.com/mock-project"
+MOCK_DEPENDENCY_NAME = "mock-dep{dep_id}"
